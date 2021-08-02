3975cdb8891c553c91340ece0a8033c74a5646d4
Ju-hyeon <mycom12317@gmail.com>
2021-08-02 22:48:47 +0900

    commit

---
+++ src/components/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <h1>Welcome {{ userId }}</h1>
+    <input v-model="newId" type="text" />
+    <button @click="updateUserId">Save</button>
+    <button @click="updateReviews">Update</button>
+    <h1>{{ reviewCount }}</h1>
+    <ul>
+      <li v-for="r in reviews" :key="r.id">
+        <p>{{ r.body }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters, mapMutations, mapActions } from "vuex";
+
+export default {
+  data() {
+    return { newId: "" };
+  },
+  computed: {
+    ...mapState({
+      userId: (state) => state.user.userId,
+    }),
+    ...mapState("reviews", {
+      reviews: (state) => state.reviews,
+    }),
+    ...mapGetters("reviews", ["reviewCount"]),
+  },
+  methods: {
+    ...mapMutations(["updateUserId"]),
+    ...mapActions("reviews", ["getReviews"]),
+    saveUserId() {
+      this.updateUserId(this.newId);
+    },
+    updateReviews() {
+      this.getReviews();
+    },
+  },
+};
+</script>
---
+++ src/components/Menus.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>Menus</h1>
+    <h1>{{ menu_name }}</h1>
+    <h1>{{ searched_menu }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Menus",
+  computed: {
+    menu_name() {
+      const id = this.$route.params.menu_id;
+      if (id == "1") return "아아";
+      else if (id == "2") return "카페라떼";
+      else return "";
+    },
+    searched_menu() {
+      const id = this.$route.query.menu_id;
+      if (id == "1") return "아아";
+      else if (id == 2) return "카패라떼";
+      else return "";
+    },
+  },
+};
+</script>
